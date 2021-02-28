c55674d8e84516a8578da7871005f91f5b20613f
Matt Caruano <mjcengineer89@yahoo.com>
2021-02-28 11:37:30 -0800

    Added new DDB-related AWS helper functions. They are still WIP

--- all-in-one/aws_utils.py
+++ all-in-one/aws_utils.py
@@ -25,3 +25,36 @@ def upload_file(file_name, bucket, object_name=None):
         logging.error(e)
         return False
     return True
+
+"""
+Uploads a record to the specified DDB table. Record must obviously
+adhere to the expected schema or it will be impossible to query.
+
+By default we declare the attribute_not_exists(TransactionID) ConditionExpression to prevent
+overwriting existing records.
+"""
+def upload_record_to_ddb_table(tableName, record):
+    ddb_client = boto3.client('dynamodb')
+    try:
+        response = ddb_client.put_item(TableName=tableName, Item=record, ConditionExpression='attribute_not_exists(TransactionID)')
+    except ClientError as e:
+        # Disregard the errors thrown from the ConditionExpression check
+        if e.response['Error']['Code'] != 'ConditionalCheckFailedException':
+            logging.error(e)
+            return False
+    return True
+
+"""
+TODO: Delete this after testing
+"""
+def query_ddb_example():
+    ddb_client = boto3.client('dynamodb')
+    try:
+        response = ddb_client.put_item(TableName=tableName, Item=record)
+        # response = ddb_client.put_item(TableName=tableName, Item=record, ConditionExpression='attribute_not_exists(TransactionID)')
+    except ClientError as e:
+        # Disregard the errors thrown from the ConditionExpression check
+        if e.response['Error']['Code'] != 'ConditionalCheckFailedException':
+            logging.error(e)
+            return False
+    return True
