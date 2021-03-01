f471a682beb5b64d95aee0956fecca1ad694dc54
Matt Caruano <mjcengineer89@yahoo.com>
2021-03-01 09:33:47 -0800

    Finished prototyping DDB query API in aws_utils. This will be used when I set up the lambda

--- all-in-one/aws_utils.py
+++ all-in-one/aws_utils.py
@@ -45,16 +45,36 @@ def upload_record_to_ddb_table(tableName, record):
     return True
 
 """
-TODO: Delete this after testing
+Central helper method for querying a table
+Query Doc: https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/dynamodb.html#DynamoDB.Client.query
+KeyConditionExpression Doc: https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/LegacyConditionalParameters.KeyConditions.html
+FilterExpression Doc: https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/LegacyConditionalParameters.QueryFilter.html
 """
-def query_ddb_example():
+def query_ddb(tableName, reverse=False):
     ddb_client = boto3.client('dynamodb')
+    response = None
     try:
-        response = ddb_client.put_item(TableName=tableName, Item=record)
-        # response = ddb_client.put_item(TableName=tableName, Item=record, ConditionExpression='attribute_not_exists(TransactionID)')
+        response = ddb_client.query(
+            TableName=tableName,
+            # IndexName=indexName,
+            # Select='ALL_PROJECTED_ATTRIBUTES', # This parameter is only availble if we specify an IndexName
+            ScanIndexForward=not reverse, # If True, will return results Ascending (AKA - not reversed)
+            KeyConditionExpression='Recipient = :recipient', # Can only reference Partiion/Sort keys from Indexes
+            FilterExpression='ContentTier = :contentTier AND DKPBefore BETWEEN :dkpLower AND :dkpUpper AND #T BETWEEN :dateLower AND :dateUpper',
+            ExpressionAttributeNames= {
+                "#T":"Timestamp" # "Timestamp" is a reserved word
+            },
+            ExpressionAttributeValues= { 
+                ":recipient":{"S":"Akaran"}, 
+                ":contentTier":{"S":"T1"},
+                ":dkpLower":{"N":"-2000"},
+                ":dkpUpper":{"N":"3000"},
+                ":dateLower":{"S":"2019-12-01T21:37:34Z"},
+                ":dateUpper":{"S":"2019-12-03T20:16:11Z"}
+            }
+        )
     except ClientError as e:
         # Disregard the errors thrown from the ConditionExpression check
         if e.response['Error']['Code'] != 'ConditionalCheckFailedException':
             logging.error(e)
-            return False
-    return True
+    return response
